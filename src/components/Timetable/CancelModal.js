c428fafe15d1f1d642046a6d9e0b8c68849f9125
superman <bainx@126.com>
2018-10-22 15:58:21 +0800

    取消课程必须设置原因

--- src/components/Timetable/CancelModal.js
+++ src/components/Timetable/CancelModal.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import {Modal, Form, Input} from 'antd';
+import {Modal, Form, Input, message} from 'antd';
 import styles from './index.less';
 
-@Form.create()
-export default class CancelModal extends Component {
+export default @Form.create()
+class CancelModal extends Component {
 
   render() {
     const {visible, onOk, onCancel, form: {getFieldDecorator, validateFieldsAndScroll}} = this.props;
@@ -12,10 +12,10 @@ export default class CancelModal extends Component {
       visible,
       onCancel,
       onOk: () => {
-        validateFieldsAndScroll((errors, payload)=>{
-          if(errors){
+        validateFieldsAndScroll((errors, payload) => {
+          if (errors) {
             console.error(errors);
-          }else{
+          } else {
             onOk && onOk(payload);
           }
         })
@@ -24,9 +24,11 @@ export default class CancelModal extends Component {
     return (
       <Modal {...modalProps}>
         <Form layout="vertical">
-          <Form.Item label="取消原因：">
+          <Form.Item label="取消原因：" required>
             {
-              getFieldDecorator('memo', {})(
+              getFieldDecorator('memo', {
+                rules: [{required: true, message: '请设置取消原因'}]
+              })(
                 <Input.TextArea autosize={{minRows: 3, maxRows: 10}}/>
               )
             }
