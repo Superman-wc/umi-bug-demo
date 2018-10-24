ef8cda95bf56683e620ec8b388796e3c0cf78434
superman <bainx@126.com>
2018-10-24 15:56:26 +0800

    ok

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,12 +7,7 @@ export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-        <li><Link to="/teacher">teacher</Link></li>
-        <Button>OK</Button>
-      </ul>
+      
     </div>
   );
 }
