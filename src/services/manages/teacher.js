05830ed221775cc6556ec1641b28e6d569ec4e8d
superman <bainx@126.com>
2019-01-04 10:52:33 +0800

    教师课表筛选，科目使用subject

--- src/services/manages/teacher.js
+++ src/services/manages/teacher.js
@@ -1,21 +1,29 @@
 import request from '../../utils/request';
 
-export async function list(data) {
-  return request('/api/8queen/teacher', { data: { s: 30, ...data }, method: 'GET' });
+/**
+ * 教师列表
+ * @param gradeId 年级ID
+ * @param subjectId 科目ID
+ * @param p 分页页码
+ * @param s 分页大小
+ * @returns {Promise<*>}
+ */
+export async function list({gradeId, subjectId, p = 1, s = 30} = {}) {
+  return request('/api/8queen/teacher', {data: {gradeId, subjectId, p, s}, method: 'GET'});
 }
 
 export async function modify(data) {
-  return request('/api/8queen/teacher/' + data.id, { data, method: 'PUT' });
+  return request('/api/8queen/teacher/' + data.id, {data, method: 'PUT'});
 }
 
 export async function create(data) {
-  return request('/api/8queen/teacher', { data, method: 'POST' });
+  return request('/api/8queen/teacher', {data, method: 'POST'});
 }
 
-export async function remove({ id }) {
-  return request('/api/8queen/teacher/' + id, { method: 'DELETE' });
+export async function remove({id}) {
+  return request('/api/8queen/teacher/' + id, {method: 'DELETE'});
 }
 
-export async function excelImport({excelUrl}){
-  return request('/api/8queen/teacher/import', {data:{excelUrl}, method:'POST'})
+export async function excelImport({excelUrl}) {
+  return request('/api/8queen/teacher/import', {data: {excelUrl}, method: 'POST'})
 }
