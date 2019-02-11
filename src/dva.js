9711b66f61573ae05bf32a2d351e846ac65cba9e
superman <bainx@126.com>
2019-02-11 11:00:22 +0800

    错误消息无法正常展示

--- src/dva.js
+++ src/dva.js
@@ -12,7 +12,7 @@ export function config() {
       err.preventDefault();
       console.error(err.message);
       notification.error({
-        message: err.message,
+        message: err.message || '系统错误，请联系管理员',
         // description: err.stack
       });
     },
